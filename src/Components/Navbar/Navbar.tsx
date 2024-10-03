b5beeae65060851cbaac9d3460dc9ca5fccc5136
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-10-03 19:38:51 +0600

    login system complate

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link href={'/api/auth/signin'} >Login</Link></li>
+                    <li>
+                        <details>
+                            <summary>Parent</summary>
+                            <ul className="bg-base-100 rounded-t-none p-2">
+                                <li><a>Link 1</a></li>
+                                <li><a>Link 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,19 +11,35 @@ const handler = NextAuth({
     providers: [
         CredentialsProvider({
             credentials: {
-                email: { label: "Email", type: "text", placeholder: "Type Your Email", require: true },
-                password: { label: "Password", type: "text", placeholder: "Type Your Password", require: true }
+                email: { label: "Email", type: "text", placeholder: "Type Your Email", required: true },
+                password: { label: "Password", type: "text", placeholder: "Type Your Password", required: true }
             },
             async authorize(credentials) {
                 if (!credentials) {
                     return null
                 }
-                return true
+                const { email, password } = credentials
+                const currentUser = users.find(user => user.email === email && user.password === password)
+                if (currentUser) {
+                    return true
+                } else { return null }
+
 
             }
         })
     ]
 
 })
+
+const users = [
+    {
+        email: "mehedi@gmail.com",
+        password: "hhhh2233"
+    },
+    {
+        emai: "rifat@gmail.com",
+        password: "Rifat1122"
+    }
+]
 
 export { handler as GET, handler as POST }
